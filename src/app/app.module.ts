5558bce8a4a5b92070097dd4bc6217c460b046bf
Anusha Charipally <anu.charipally@gmail.com>
2018-10-24 18:02:37 -0700

    spa part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule} from './app-routing/app-routing.module';
 
@@ -43,7 +44,10 @@ import { AppRoutingModule} from './app-routing/app-routing.module';
     MatCardModule,
     MatButtonModule
   ], 
-  providers: [DishService],
+  providers: [
+    DishService,
+    PromotionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
